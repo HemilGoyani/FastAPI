99b6c881b4e01f4ac55d1e5f99fd16f3e554ba16
Hemil Goyani <goyanihemil8@gmail.com>
2022-01-09 21:14:53 -0800

    crud operation fast api is saved

--- database_API/my_super_project/Async_app/main.py
+++ database_API/my_super_project/Async_app/main.py
@@ -1,45 +1,83 @@
 from typing import List, Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.param_functions import Depends
-from pydantic import BaseModel,EmailStr
-from sqlalchemy import Column,String, Integer, Boolean
+from pydantic import BaseModel, EmailStr
+from sqlalchemy import Column, String, Integer, Boolean
 from sqlalchemy.orm.session import Session
-from database import Base,engine,SessionLocal
+from sqlalchemy.sql.functions import user
+from database import Base, engine, SessionLocal
 
-#models
+# models
+
+
 class User(Base):
-    __tablename__="Users"
-    id=Column(Integer,primary_key=True, index=True)
-    email=Column(String,unique=True,index=True)
-    is_active=Column(Boolean,default=True)
+    __tablename__ = "Users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    is_active = Column(Boolean, default=True)
 
-#schemas
+# schemas
+
+
 class UserSchemas(BaseModel):
-    id: int
-    email: EmailStr  
-    is_active:bool
-    
+    email: str
+    is_active: bool
+
     class Config:
-         orm_mode = True 
-         
+        orm_mode = True
+
+
+class GetUserSchemas(BaseModel):
+    id: Optional[int]
+    email: Optional[str]
+    is_active: Optional[bool]
+
+    class Config:
+        orm_mode = True
+
+
 def get_db():
-     db = SessionLocal()
-     try:
-         yield db
-     finally:
-         db.close()
-        
-                 
-Base.metadata.create_all(bind = engine)
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-@app.post("/users",response_model=UserSchemas)
-async def index(user:UserSchemas,db:Session=Depends(get_db)):
-    u = User(id=user.id,email=user.email,is_active=user.is_active)
+
+@app.post("/users", response_model=GetUserSchemas)
+async def index(user: UserSchemas, db: Session = Depends(get_db)):
+    u = User(email=user.email, is_active=user.is_active)
     db.add(u)
     db.commit()
     return u
 
-@app.get("/users/",response_model= List[UserSchemas])
-async def index(db:Session=Depends(get_db)):
+
+@app.get("/users/", response_model=List[GetUserSchemas])
+async def index(db: Session = Depends(get_db)):
     return db.query(User).all()
+
+
+@app.put("/users/{user_id}", response_model=GetUserSchemas)
+async def update_user(user_id: int, data: UserSchemas, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id)
+    users = user.first()
+    if not users:
+        raise HTTPException(status_code=404, detail="id not found")
+    user.update({"email": data.email, "is_active": data.is_active})
+    db.commit()
+    return users
+
+
+@app.delete("/users/{user_id}")
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id)
+
+    if not user.first():
+        raise HTTPException(status_code=404, detail="id not found")
+    user.delete(synchronize_session=False)
+    db.commit()
+    return "record are deleted"
